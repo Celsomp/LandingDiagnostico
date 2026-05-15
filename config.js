f5688d9e84fe44626bca8e24558f5b5bc63acdbc
Celso Pereira <celsopereira79@gmail.com>
2026-05-15 07:15:20 +0100

    remove config.js do tracking do git

--- config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-  supabase: {
-    url: 'https://ofxdnczrjgducczbxrxn.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meGRuY3pyamdkdWNjemJ4cnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3ODAyMTYsImV4cCI6MjA5NDM1NjIxNn0.t_dziyycj717iCOzOm17JNQ7hfiflJSYN6iW8a0iUVE'
-  },
-  systemeio: {
-    key: '0a83hgdvopdrfhy32tgptxyf6ajaalmqnno6l4cq4iejetil9rsuoem57evk3xbu'
-  }
-};
